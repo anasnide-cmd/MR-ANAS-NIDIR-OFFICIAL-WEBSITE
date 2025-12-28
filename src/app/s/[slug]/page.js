6dbc0432b02198f7b0196c55dd2f24cb4c8656ac
anasnide <anasnidirproject@gmail.com>
2025-12-29 00:02:49 +0800

    refactor: Update static parameter generation in UserSitePage to return a placeholder for dynamic routing

--- src/app/s/[slug]/page.js
+++ src/app/s/[slug]/page.js
@@ -1,14 +1,13 @@
 import UserSiteClient from './UserSiteClient';
 
 // For static export, we need to provide generateStaticParams
-// Since sites are created dynamically, we return empty array
-// and handle routing client-side with Firebase Hosting rewrites
-export async function generateStaticParams() {
-    // Return empty array - pages will be generated on-demand
-    // This allows the route to work with static export
-    // Firebase Hosting rewrites will handle routing to this page
-    return [];
-}
+// Return at least one placeholder param to ensure route is generated
+// Actual sites will be handled client-side via Firebase queries
+export const generateStaticParams = async () => {
+    // Return a placeholder - actual routing handled client-side
+    // Firebase Hosting rewrites will route all /s/* to this page
+    return [{ slug: 'placeholder' }];
+};
 
 export default function UserSitePage({ params }) {
     return <UserSiteClient slug={params.slug} />;
