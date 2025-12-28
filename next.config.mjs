d7a63f73460d0aee0a14f1d4785fec4818a8d120
anasnide <anasnidirproject@gmail.com>
2025-12-28 18:43:59 +0800

    feat: Add new admin page and update Next.js configuration.

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: { unoptimized: true },
-  output: 'export'
+  output: 'export',
+  trailingSlash: true
 };
 
 export default nextConfig;
